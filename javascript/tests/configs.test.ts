374df4e634f5f6ec5a9374a07ad8bb0530023845
amochkin <amochkin@gmail.com>
2024-10-01 11:53:07 +0200

    feat: updated tests

--- javascript/tests/configs.test.ts
+++ javascript/tests/configs.test.ts
@@ -1,5 +1,5 @@
 /**
- * 2023 Copyright P8 Enterprise Components, Inc.
+ * 2024 Copyright P8 Enterprise Components, Inc.
  * All Rights Reserved.
  */
 
@@ -7,14 +7,17 @@ import { eslintConfigRecommended } from '../src';
 import { prettierConfigRecommended } from '../src';
 
 const configs = [
-	{ name: 'eslintConfigRecommended', config: eslintConfigRecommended },
-	{ name: 'prettierConfigRecommended', config: prettierConfigRecommended },
+	{ name: 'eslintConfigRecommended', config: eslintConfigRecommended() },
+	{ name: 'prettierConfigRecommended', config: prettierConfigRecommended() },
 ];
 
-describe('Config is not empty', () => {
+describe('Test config factories', () => {
 	configs.forEach(({ name, config }) => {
 		test(name, () => {
 			expect(config).not.toBeNull();
+			expect(config).not.toBeFalsy();
+			expect(config).toBeDefined();
+			expect(typeof config === 'function').toBe(false);
 		});
 	});
 });
