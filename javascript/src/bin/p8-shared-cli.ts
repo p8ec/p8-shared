708dfe414e4ed0072408dfed58fe9a108b3cbc6b
amochkin <amochkin@gmail.com>
2026-01-11 19:26:23 +0100

    refactor: modularized CLI utilities and added `run` command with workspace mode support

--- javascript/src/bin/p8-shared-cli.ts
+++ javascript/src/bin/p8-shared-cli.ts
@@ -18,23 +18,27 @@ import yesno from './utils/yesno';
 import prompt from './utils/prompt';
 import * as child_process from 'node:child_process';
 
-const IS_DEV = process.env.NODE_ENV === 'development';
+export const IS_DEV = process.env.NODE_ENV === 'development';
 
 let args = processArgs.args;
 const self = path.parse(processArgs.name).name;
 const writeLn = console.log;
-const execShell = (command: string) =>
-	IS_DEV ? writeLn(`DEV: execShell ${command}`) : child_process.execSync(command).toString();
-const writeFile = (name: string, data: string) =>
-	IS_DEV ? writeLn(`DEV: writeFile name=${name} data=${data}`) : fs.writeFileSync(path.join(process.cwd(), name), data);
-const copyAsset = (name: string) =>
-	IS_DEV
-		? writeLn(`DEV: copyAsset name=${name}`)
-		: fs.copyFileSync(path.join(__dirname, '..', '..', 'assets', name), path.join(process.cwd(), name));
+export const cliUtils = {
+	execShell: (command: string) =>
+		IS_DEV ? writeLn(`DEV: execShell ${command}`) : child_process.execSync(command).toString(),
+	writeFile: (name: string, data: string) =>
+		IS_DEV
+			? writeLn(`DEV: writeFile name=${name} data=${data}`)
+			: fs.writeFileSync(path.join(process.cwd(), name), data),
+	copyAsset: (name: string) =>
+		IS_DEV
+			? writeLn(`DEV: copyAsset name=${name}`)
+			: fs.copyFileSync(path.join(__dirname, '..', '..', 'assets', name), path.join(process.cwd(), name)),
+};
 
-if (args.length === 0 && !IS_DEV) {
+if (args.length === 0 && !IS_DEV && require.main === module) {
 	writeLn(`
-Usage: ${self} [command] [options]
+Usage: ${self} {command} [options]
 
 Commands:
 	init [cleanup]
@@ -45,6 +49,20 @@ Commands:
 		Returns the directory name of the caller.
 			Options:
 				levelsUp: The number of levels up to return the directory name.
+	run [script [packageManager [workspaceMode]]]
+		Returns a command to run a script with the specified package manager.
+			Options:
+				script: The script to run.
+					- 'start': Starts the application.
+					- 'build': Builds the application.
+					- 'test': Runs tests.
+					- 'lint': Lints the application.
+					- 'start-workspace': Starts the workspace.
+					- 'build-workspace': Builds the workspace.
+					- 'test-workspace': Runs tests in the workspace.
+					- 'lint-workspace': Lints the workspace.
+				packageManager: The package manager to use (npm, yarn, pnpm). Defaults to npm.
+				workspaceMode: Whether to run in workspace mode (seq or par for pnpm). Defaults to none.
 `);
 
 	if (IS_DEV) {
@@ -53,7 +71,7 @@ Commands:
 	process.exit(1);
 }
 
-const initCleanup = (packageJson: Record<string, unknown>): void => {
+export const initCleanup = (packageJson: Record<string, unknown>): void => {
 	writeLn('Removing eslintConfig and prettier from package.json...');
 	const configBackup: Record<string, unknown> = {};
 	const configBackupFile = 'p8-package-backup.json';
@@ -67,23 +85,25 @@ const initCleanup = (packageJson: Record<string, unknown>): void => {
 		}
 	});
 
-	writeFile(`${configBackupFile}`, JSON.stringify(configBackup, null, 2));
-	writeFile('package.json', JSON.stringify(packageJson, null, 2));
+	cliUtils.writeFile(`${configBackupFile}`, JSON.stringify(configBackup, null, 2));
+	cliUtils.writeFile('package.json', JSON.stringify(packageJson, null, 2));
 };
 
 /**
  * Initializes a TypeScript project with P8 shared configurations.
  */
-const init = async (option: string) => {
-	const packageJson = JSON.parse(String(fs.readFileSync(path.join(process.cwd(), 'package.json'))));
+export const init = async (option: string) => {
+	const packageJsonData = fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8');
+	const packageJson = JSON.parse(packageJsonData);
 	const moduleType = packageJson['type'] === 'module' ? 'mjs' : 'cjs';
 
 	writeLn(`Creating eslint.config.${moduleType}...`);
-	copyAsset(`eslint.config.${moduleType}`);
+	cliUtils.copyAsset(`eslint.config.${moduleType}`);
 
 	writeLn(`Creating prettier.config.${moduleType}...`);
-	copyAsset(`prettier.config.${moduleType}`);
+	cliUtils.copyAsset(`prettier.config.${moduleType}`);
 
+	packageJson.scripts ??= {};
 	packageJson.scripts['npm:reset'] = 'rm -rf ./**/node_modules && rm -rf ./**/package-lock.json && npm install';
 	packageJson.scripts['npm:audit'] = 'npm audit --audit-level=moderate';
 
@@ -96,10 +116,10 @@ const init = async (option: string) => {
 
 	if (lefthook) {
 		writeLn(`Creating commitlint.config.${moduleType}...`);
-		copyAsset(`commitlint.config.${moduleType}`);
+		cliUtils.copyAsset(`commitlint.config.${moduleType}`);
 
 		writeLn('Creating lefthook.yml...');
-		copyAsset('lefthook.yml');
+		cliUtils.copyAsset('lefthook.yml');
 		writeLn('Adding lefthook install to postinstall...');
 		const lefthookInstall = 'lefthook install';
 		packageJson.scripts.postinstall = lefthookInstall;
@@ -115,7 +135,7 @@ const init = async (option: string) => {
 			})
 		) {
 			writeLn(`Executing ${npmInstall}...`);
-			execShell(npmInstall);
+			cliUtils.execShell(npmInstall);
 		} else {
 			writeLn('You could run the following command to install needed dependencies:');
 			writeLn(npmInstall);
@@ -132,7 +152,7 @@ const init = async (option: string) => {
 			})
 		) {
 			writeLn(`Executing ${lefthookInstall}...`);
-			execShell(lefthookInstall);
+			cliUtils.execShell(lefthookInstall);
 		}
 	}
 
@@ -140,14 +160,14 @@ const init = async (option: string) => {
 		initCleanup(packageJson);
 	} else {
 		writeLn('Skipping cleanup...');
-		writeFile('package.json', JSON.stringify(packageJson, null, 2));
+		cliUtils.writeFile('package.json', JSON.stringify(packageJson, null, 2));
 	}
 };
 
 /**
  * Returns the directory name of the caller, optionally returns a directory name specified levels up.
  */
-const dirn = (levelsUp: string): string => {
+export const dirn = (levelsUp: string): string => {
 	const DEFAULT_LEVELS_UP = 0;
 	levelsUp ??= `${DEFAULT_LEVELS_UP}`;
 
@@ -156,7 +176,41 @@ const dirn = (levelsUp: string): string => {
 	return process.cwd().split(path.sep).reverse()[levels];
 };
 
-const setup = async () => {
+export const run = (script: string, packageManager = 'npm', workspaceMode = 'none'): string => {
+	const pnpmWorkspaceSeq = '-r --workspace-concurrency=1 --if-present --reporter-hide-prefix';
+	const pnpmWorkspacePar = '-r --if-present --parallel';
+	const yarnWorkspaceSeq = 'workspaces foreach -A';
+	const yarnWorkspacePar = 'workspaces foreach -A -p';
+
+	const commands = {
+		npm: {
+			none: `npm run ${script}`,
+			seq: `npm run ${script} --workspaces --if-present`,
+		},
+		yarn: {
+			none: `yarn run ${script}`,
+			seq: `yarn ${yarnWorkspaceSeq} run ${script}`,
+			par: `yarn ${yarnWorkspacePar} run ${script}`,
+		},
+		pnpm: {
+			none: `pnpm run ${script}`,
+			seq: `pnpm ${pnpmWorkspaceSeq} run ${script}`,
+			par: `pnpm ${pnpmWorkspacePar} run ${script}`,
+		},
+	};
+
+	if (!commands[packageManager as never]) {
+		throw new Error(`Unknown package manager: ${packageManager}`);
+	}
+
+	if (!commands[packageManager as never]?.[workspaceMode]) {
+		throw new Error(`Unknown workspace mode: ${workspaceMode}`);
+	}
+
+	return (commands[packageManager as never] as never)[workspaceMode];
+};
+
+const main = async () => {
 	// Ask the user for arguments if IS_DEV is true
 	if (IS_DEV) {
 		args = (await prompt('Enter arguments: ')).split(' ');
@@ -169,22 +223,27 @@ const setup = async () => {
 		case 'dirn':
 			writeLn(dirn(args[1]));
 			break;
+		case 'run':
+			writeLn(run(args[1], args[2]));
+			break;
 		default:
 			console.error(`Unknown command: ${args[0]}`);
 			process.exit(1);
 	}
 };
 
-setup()
-	.then((r) => {
-		if (IS_DEV) {
-			writeLn(`DEV: setup completed with result: ${r}`);
-		}
-	})
-	.catch((err) => {
-		if (IS_DEV) {
-			writeLn(`DEV: setup failed with error: ${err}`);
-		}
-		console.error(`Error: ${err}`);
-		process.exit(1);
-	});
+if (require.main === module) {
+	main()
+		.then((r) => {
+			if (IS_DEV) {
+				writeLn(`DEV: setup completed with result: ${r}`);
+			}
+		})
+		.catch((err) => {
+			if (IS_DEV) {
+				writeLn(`DEV: setup failed with error: ${err}`);
+			}
+			console.error(`Error: ${err}`);
+			process.exit(1);
+		});
+}
